ad019e2b7a607bb07138aaac0e22bd8e87b7a1fb
fatima <fatimahassanhassab8@gmail.com>
2024-03-10 06:15:23 -0700

    adding a new endpoint students

--- api.js
+++ api.js
@@ -11,9 +11,14 @@ const express = require("express");
 const app = express();
 var cors = require('cors')
 app.use(cors())
-app.get('/', function (req, res) {
+app.get('/students', function (req, res) {
+  data=[{id:150,name:'ali'},{id:15,name:'hassam'}]
+    res.send(data);
+  })
+  app.get('/', function (req, res) {
     res.send('Hello World')
   })
+
 const PORT = process.env.PORT || 3030;
 
 // your code
